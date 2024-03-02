fa569db28d28e77d14d54e84acfecd3fa63c4a37
Fifciuu <filip.jdrasik@gmail.com>
2024-03-02 12:25:09 +0100

    refactor: cleaner code on sign up view

--- client-vue/src/views/SignUpView.vue
+++ client-vue/src/views/SignUpView.vue
@@ -1,34 +1,16 @@
 <script setup lang="ts">
-import LandingHeader from '@/components/Landing/LandingHeader.vue';
 import LandingCTA from '@/components/Landing/LandingCTA.vue';
 import SignUpForm from '@/components/Forms/SignUpForm.vue';
-import BaseButton from '@/components/Base/BaseButton.vue';
 </script>
 
 <template>
-  <div class="contet">
+  <div>
     <LandingCTA title="Shorten, Share, Control"
       description="Maximum Functionality in Short Links - See How Easy You Can Shorten URLs!"
-      class="lg:pl-0 xl:pl-17 lg:ml-17" />
+      class="d-hidden lg:d-inline lg:mt-6 lg:pl-0 xl:pl-17 lg:ml-17" />
     <img src="@/assets/mobile-path.svg" class="absolute -top-4 pointer-events-none lg:hidden" />
     <SignUpForm />
   </div>
 </template>
 
-<style lang="scss" scoped>
-.header {
-  position: relative;
-  z-index: 2;
-}
-
-.cta {
-  display: none;
-}
-
-@media only screen and (min-width: $widthDesktop) {
-  .cta {
-    display: inline-block;
-    margin-top: 48px;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
